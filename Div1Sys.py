801fc8c1488ca68d1b56b034a4d74a1108e62985
JustHackedTV <78915697+JustHackedTV@users.noreply.github.com>
2022-03-23 06:56:07 -0300

    Update Div1Sys.py

--- Div1Sys.py
+++ Div1Sys.py
@@ -3,30 +3,20 @@ import asyncio
 import json
 
 def ApplyDiv1(client, GUILDS):
-    @client.command(name="applydiv1", guild_ids=GUILDS)
-    async def _applydiv1(ctx):
-        if ctx.channel.id == 885613928858665040:
-            await ctx.respond("Caregando Perguntas", ephemeral=True)
-            def check(m):
-                return m.author == ctx.author and m.channel == ctx.channel
-            embed1 = discord.Embed(title=f"Escreva Abaixo seu Username no minecraft:")
-            embed2 = discord.Embed(title=f"Escreva o motivo de sua aplicação, caso tenho prints de sua win conta um Div 1, coloque em sua resposta (Você tem 15 minutos, caso queira mais tempo, escreva primeiro, depois envie).")
-            questions = [embed1, embed2]
-            answers = []
-            for i in questions:
-                q_msg = await ctx.send(embed=i)
-                try:
-                    msg = await client.wait_for('message', timeout=900.0, check=check)
-                except asyncio.TimeoutError:
-                    await q_msg.delete()
-                    return await ctx.respond("Você falhou em responder a pergunta a tempo.")
-                else:
-                    await q_msg.delete()
-                    answers.append(msg.content)
-                    await msg.delete()
-    
-            JsonOb = json.load(open("Div1Apps.json", "r"))
-            JsonOb[str(ctx.author.id)] = {}
-            JsonOb[str(ctx.author.id)]["App"] = answers[1]
-            JsonOb[str(ctx.author.id)]["IGN"] = answers[0]
-            json.dump(JsonOb, open("Div1Apps.json", "w"), indent=4, sort_keys=True)
+    @client.command(name="apply", description="comando para aplicar pra divisão 1", guild_ids=GUILDS)
+    async def app_(ctx):
+        modal = discord.ui.Modal(title="Aplicação para Divisão 1:")
+        modal.add_item(discord.ui.InputText(label=f"Escreva seu nick na caixa abaixo:", style=discord.InputTextStyle.short, placeholder="IGN"))
+        modal.add_item(discord.ui.InputText(label="Escreva o motivo para você ser Divisão 1:", style=discord.InputTextStyle.long))
+        async def callback(interaction):
+            resposta1 = modal.children[0].value
+            resposta2 = modal.children[1].value
+            load = json.load(open("Div1Apps.json", "r"))
+            load[str(ctx.author.id)] = {}
+            load[str(ctx.author.id)]["IGN"] = resposta1
+            load[str(ctx.author.id)]["Motivo"] = resposta2
+            json.dump(load, open("Div1Apps.json", "w"), indent=4, sort_keys=True)
+            await interaction.response.send_message(f""":tada: **Sucesso!**
+`{ctx.author.name}` sua aplicação foi enviada com sucesso!""", ephemeral=True)
+        modal.callback = callback
+        await ctx.send_modal(modal)
